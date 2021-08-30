729b3aed4922dd339e991a799a8787b2b36baf81
CodingFever <dannelhdx@gmail.com>
2021-08-30 00:18:47 -0400

    initial the board

--- GreedySnake_C++/main.cpp
+++ GreedySnake_C++/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
-#include <conio.h>
-#include <ctime>
+
+using namespace std;
 
 #define WIDTH 50
 #define HEIGHT 25
+
+//initial the playing background
+void board(){
+	for (int i = 0; i < HEIGHT; i++){
+		cout << "#";
+		for(int j = 0; j < WIDTH; j++){
+			if(i == 0|| i == HEIGHT-1){cout << "#";}
+			else{cout << " ";}
+		}
+		cout << "#\n";
+	}
+}
+
+int main(){
+	board();
+	return 0;
+}
