f3e6a483667346cde7eeb0f7f9c626fab8a50764
CodingFever <dannelhdx@gmail.com>
2021-09-06 22:59:46 -0400

    point genetrted

--- GreedySnake_C/snake.c
+++ GreedySnake_C/snake.c
@@ -21,7 +21,7 @@ enum eDirection
 };
 //get snake's pos for each part
 position_list snake_pos[120];
-
+position_list Point;
 int direction = RIGHT; //snake default moving direction
 int ch;                //input
 int speed = 500;
@@ -54,10 +54,11 @@ void Game_Page(offsetx,offsety)
     //wait for the game start
     while (getch() != 's')
     {
-        if (getch() == 'q')
-        {
-            break;
-        }
+        
+        //if (getch() == 'q')
+        //{
+        //    break;
+        //}
         wclear(board);
         mvprintw(offsety + HEIGHT / 2-5, offsetx+19, "Greedy Snake Game");
         mvprintw(offsety + HEIGHT / 2, offsetx+19, "Enter 'S' to start");
@@ -70,17 +71,51 @@ void Game_Page(offsetx,offsety)
 
 void CreateSnake(offsetx,offsety)
 {
+    wclear(board);
+
     for(int i = 0;i < SnakeLength;i++){
         snake_pos[i].x = offsetx+10-i;
         snake_pos[i].y = offsety;
+    }
+    mvwaddch(board, snake_pos[0].y, snake_pos[0].x, '0');
+    for(int i = 1;i < SnakeLength;i++){
+        //snake body initial print
+        mvwaddch(board, snake_pos[i].y, snake_pos[i].x, '#');
     }
     mvprintw(4, WIDTH / 2 + 11, "Score: %d", Score);
     box(board,0,0);
     wrefresh(board);
 }
+bool Generat_Point(){
+    srand(time(NULL)); 
+    int Point_y = (rand() % (HEIGHT-1));
+    int Point_x = (rand() % (WIDTH-1));
+    for(int i = 0;i<SnakeLength;i++ ){
+        //generated on on snake's body
+        if(snake_pos[i].x == Point_x && snake_pos[i].y == Point_y){
+            return false;
+        }
+        //generated on the border
+        else if(Point_y == 0 ||Point_x == 0){
+            return false;
+        }
+    }
+    
+        Point.y = Point_y;
+        Point.x = Point_x;
+        return true;
+}
 void Game(offsetx,offsety)
 {
     CreateSnake(offsetx,offsety);
+    while(Generat_Point()){
+        while ((ch = getch()) != 'q') {
+            wclear(board);
+            mvprintw(1,offsetx+HEIGHT/2+10, "Score: %d ",Score);
+            box(board, 0 , 0);
+            wrefresh(board);
+        }
+    }
 }
 
 int main()
@@ -89,6 +124,10 @@ int main()
     int offsetx = (COLS - WIDTH) / 2;
     int offsety = (LINES - HEIGHT) / 2;
     Game_Page(offsetx,offsety);
-    Game(offsetx,offsety);
+    if(getch() == 's'){
+
+        Game(offsetx,offsety);
+    }
+    
     return 0;
 }
